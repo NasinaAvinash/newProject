76c724b46bb39956d9739074bc588cf0b25ce0b6
DELL <DELL@DESKTOP-U0T0RH6>
2023-04-14 08:08:55 -0700

    second commit...

---
+++ src/package2/Class2.java
@@ -0,0 +1,19 @@
+package package2;
+
+public class Class2 { 
+         
+	public static void main(String[] args) {
+		String name ="nasina";
+		if(name == "nasina") {
+			 System.out.println("lover boy");
+		}
+		else {
+			System.out.println("good boy");
+		}
+			
+		}
+	
+
+	
+
+}
